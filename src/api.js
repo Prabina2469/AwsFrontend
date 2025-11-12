8f35fdb9cb0d166c3754cea71de6ef7858195544
prabina2469 <prabina2001@gmail.com>
2025-11-12 11:56:52 -0500

    again am updated

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://13.222.191.198:8080";
+const API_URL = "https://13.222.191.198:8080";
 
 export const getTodos = () => axios.get(API_URL);
 export const addTodo = (todo) => axios.post(API_URL, todo);
